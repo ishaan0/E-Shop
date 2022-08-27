13b00f5ae8abdd65fd895af0a591a46b71ee9f90
iftekharul.alam@selise.ch <iftekharctgbd@gmail.com>
2022-08-27 18:12:06 +0600

    cart service data fixed

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -8,20 +8,7 @@ import { Product } from '../component/products/product.model';
 export class CartService {
   productChanged = new Subject<Product[]>() ;
 
-  products: Product[] = [{
-    title: 'tesing title',
-    price: 20.34,
-    description: 'This is testing description',
-    imagePath:
-      'https://media.istockphoto.com/photos/blank-black-tshirt-front-with-clipping-path-picture-id483960103?b=1&k=20&m=483960103&s=170667a&w=0&h=hNKNseCmaThTsh4i7Q3kHETlWo5Zi7Ogw-luVozfP_M=',
-  },
-  {
-    title: 'tesing title',
-    price: 20.34,
-    description: 'This is testing description',
-    imagePath:
-      'https://media.istockphoto.com/photos/blank-black-tshirt-front-with-clipping-path-picture-id483960103?b=1&k=20&m=483960103&s=170667a&w=0&h=hNKNseCmaThTsh4i7Q3kHETlWo5Zi7Ogw-luVozfP_M=',
-  }];
+  products: Product[] = [];
 
   constructor() { }
 
